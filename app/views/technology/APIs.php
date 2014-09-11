3dd075a81d5ab21b36a08abf52c5d7a3c4351421
unknown <ankit.s@imaginea.com>
2014-09-11 19:18:03 +0530

    imaginea new page

---
+++ app/views/technology/APIs.php
@@ -0,0 +1,155 @@
+@extends('templates.default.default')
+
+@section('page-title')
+Imaginea Technology
+@stop
+@section('body-tag')
+technology technology_soc
+@stop
+@section('banner-right')
+<p>
+	We understand social platforms, collaboration issues, and how to build social applications.
+</p>
+@stop
+@section('main-content-left') <h1 class="orange">Social Technologies </h1>
+<h2> Thinking about social media application strategy but unsure where to start? We’ve
+helped clients throughout the process: </h2>
+
+<h5>Why APIs for Enterprise</h5>
+<p>The thumping success of APIs in the consumer space has pushed APIs to be part of emerging platforms. 
+
+After seeing huge success of API-based development and subsequent growth in social media and 
+
+consumer space, in general, enterprises are now keen to replicate that in their business.. The new API 
+
+architecture is transforming the way business is done. For, APIs are not just an aid to achieve their 
+
+business goals, but are core to the business strategy. APIs are opening up newer business channels and 
+
+speeding up partner interactions. Internally too, API adoption is helping processes keep pace with 
+
+business change. </p>
+
+<p>Enterprises are compelled to create newer applications that appeal to different markets and 
+
+customer segments, and make them accessible to a variety of user-agents. Visual representation and 
+
+communication of data and information . Cloud applications need a standard way to circle back data and 
+
+functions within internal systems. Achieving all these is much easier and quicker with APIs.</p>
+
+<p>While it is currently trending in enterprises, APIs havealways existed. The current day web API 
+
+architecture provides a loose coupling and extensive reuse. While this makes development fater, a 
+
+note of caution: enterprises do not need APIs that are isolated and are not in line with business needs. 
+
+Enterprises need a holistic approach to include APIs in their business strategy. And more so, to make 
+
+APIs their business.
+</p>
+
+
+<h5>Building good APIs</h5>
+<p>Badly designed APIs can create mess and can prove to be more expensive than its worth. Key 
+
+design features mark good APIs. Strong API architecture and design is critical to ensure the right 
+
+implementation of these key features.</p>
+<p>With good understanding of the enterprises’ API strategy, use of best practices, sufficient integration 
+
+expertise, your business can serve a as a good base for creating a robust API design.</p>
+
+<h5>Why engineering is critical for APIs</h5>
+<p>Often asked: if APIs are about interface and integration, is engineering critical for APIs? Absolutely!
+
+Strong engineering and development minds are needed for:</p>
+
+<dl>
+	
+	<dd>
+		<ul class="dotted">
+<li>Creating a sustainable low level design for APIs that can reduce the actual development time</li>
+
+<li>Evaluating and choosing a perfect implementation technology stack for APIs</li>
+
+<li>Optimizing and performing APIs</li>
+
+<li>Automatingtests for the APIs</li>
+</ul>
+	</dd>
+</dl>
+
+<p>Only good engineering can ensure a good developer experience. APIs are all about Developer Experience 
+
+(DX).</p>
+
+<h5>Do I really need to manage my APIs? Is Management tool a must?</h5>
+<p>Well, yes. Some level of management is required to avoid chaos. The relevance of APIs to core business 
+
+and existing IT landscape largely drives the those management needs. For instance, new APIs that 
+
+need security need authentication and authorization logic, which might already exist in the enterprise 
+
+IAM. This is an obvious need to integrate with the enterprise IAM. There can be another set of APIs that 
+
+need some access limitation policies. While these can probably be managed from an API publishing 
+
+application, these are wide concerns and better solved in a standard way.</p>
+
+<p>WaveMaker Gateway helps in handling all the general concerns that comes along with APIs. Imaginea is 
+
+a preferred professional services provider for WaveMaker Gateway.</p>
+
+<p>Tools can certainly manage APIs but only to the extent it is configured. Enterprises most certainly need 
+
+experts to evaluate and configure the best tool for their purpose. We also offer services on other API 
+
+Management Tools</p>
+
+<p>A complete API Management product can be very useful but is not mandatory to venture into APIs.
+
+Nevertheless, even the simplest API implementation will need ways to make the APIs discoverable, 
+
+understandable, and at least minimally secured.</p>
+
+<p>Simply get started with WaveMaker Gateway Lite. Postpone your worries about procuring a commercial
+
+API Management tool even before you are sure about your APIs.</p>
+
+<p>You may have APIs but creating API driven smart web & mobile applications is time consuming 
+
+and difficult without the right tools.</p>
+<p>Many enterprises are in a state that is unable to realize the investments made in APIs. Using a tool 
+
+like Wavemaker Stydio, creating API driven applications to be served on web and mobile is no longer 
+
+time consuming or difficult. The Studio is a visual RAD tool that offers you ways to drag-and-drop UI 
+
+components, connect to web services, and create database schemas -- without writing any code.</p>
+
+<h5>Our Experience with APIs</h5>
+
+<p>WaveMaker, Inc. provides App Enablement Suite (AES), an end-to-end solution for publishing, managing 
+
+APIs and uniquely promoting API consumption. WaveMaker improves API consumption by empowering 
+
+the large population of user developers, as well as professional developers, to create API apps.</p>
+
+<p>WaveMaker is a portfolio company of Pramati Technologies. Imaginea has been serving as the 
+
+engineering arm for the WaveMaker product suite. Building a commercial grade API Management 
+
+Gateway has given us immense insight in to different aspect of API building, publishing and consuming.</p> 
+
+<p>The product DNA in Imaginea has always ensured high quality deliverables and very happy customers. </p>
+
+<p>Want to be happy too?</p>
+
+
+
+@stop
+@section('main-content-right')
+@include('technology.sub_nav')
+@include('about.sub_mainstrip_right')
+@stop
