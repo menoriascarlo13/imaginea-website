f365374ccc4e2523f6d08bec3fc3b98377eb20af
unknown <ankit.s@imaginea.com>
2014-09-12 14:42:43 +0530

    imaginea new page edit

--- app/views/technology/APIs-temp89.blade.php
+++ app/views/technology/APIs-temp89.blade.php
@@ -11,9 +11,7 @@ technology technology_soc
 	We understand social platforms, collaboration issues, and how to build social applications.
 </p>
 @stop
-@section('main-content-left') <h1 class="orange">Social Technologies </h1>
-<h2> Thinking about social media application strategy but unsure where to start? We’ve
-helped clients throughout the process: </h2>
+@section('main-content-left') 
 
 <h5>Why APIs for Enterprise</h5>
 <p>The thumping success of APIs in the consumer space has pushed APIs to be part of emerging platforms. 
@@ -24,29 +22,29 @@ consumer space, in general, enterprises are now keen to replicate that in their 
 
 architecture is transforming the way business is done. For, APIs are not just an aid to achieve their 
 
-business goals, but are core to the business strategy. APIs are opening up newer business channels and 
+business goals, but are core to the business strategy. APIs are opening up newer business channels 
 
-speeding up partner interactions. Internally too, API adoption is helping processes keep pace with 
+and speeding up partner
 
-business change. </p>
+business change.</p>
 
-<p>Enterprises are compelled to create newer applications that appeal to different markets and 
+<p>Enterprises are compelled to create newer applications that appeal to different markets and
 
 customer segments, and make them accessible to a variety of user-agents. Visual representation and 
 
-communication of data and information . Cloud applications need a standard way to circle back data and 
+communication of data and information Cloud applications need a standard way to circle back data and 
 
 functions within internal systems. Achieving all these is much easier and quicker with APIs.</p>
 
-<p>While it is currently trending in enterprises, APIs havealways existed. The current day web API 
+<p>While it is currently trending in enterprises, APIs have always existed. The current day web API 
 
-architecture provides a loose coupling and extensive reuse. While this makes development fater, a 
+architecture provides a loose coupling and extensive reuse. While this makes development faster, a 
 
 note of caution: enterprises do not need APIs that are isolated and are not in line with business needs. 
 
-Enterprises need a holistic approach to include APIs in their business strategy. And more so, to make 
+Enterprises need a holistic approach to include APIs in their business strategy; more so, to make APIs 
 
-APIs their business.
+their business.
 </p>
 
 
@@ -56,6 +54,8 @@ APIs their business.
 design features mark good APIs. Strong API architecture and design is critical to ensure the right 
 
 implementation of these key features.</p>
+<img src="images/wavemaker/api-design.png" />
+
 <p>With good understanding of the enterprises’ API strategy, use of best practices, sufficient integration 
 
 expertise, your business can serve a as a good base for creating a robust API design.</p>
@@ -75,7 +75,7 @@ Strong engineering and development minds are needed for:</p>
 
 <li>Optimizing and performing APIs</li>
 
-<li>Automatingtests for the APIs</li>
+<li>Automating tests for the APIs</li>
 </ul>
 	</dd>
 </dl>
@@ -97,54 +97,61 @@ need some access limitation policies. While these can probably be managed from a
 
 application, these are wide concerns and better solved in a standard way.</p>
 
-<p>WaveMaker Gateway helps in handling all the general concerns that comes along with APIs. Imaginea is 
+<p>Imaginea is partnering best of breed API products in the market so that its customers can realize the full 
 
-a preferred professional services provider for WaveMaker Gateway.</p>
+potential of APIs. WaveMaker Gateway helps in handling all the general concerns that comes along with 
 
-<p>Tools can certainly manage APIs but only to the extent it is configured. Enterprises most certainly need 
+APIs. Imaginea is a preferred professional services provider for WaveMaker Gateway.</p>
 
-experts to evaluate and configure the best tool for their purpose. We also offer services on other API 
+<img src="images/wavemaker/logo-gateway.png" />
+<p>Tools can certainly manage APIs but only to the extent it is configured. Enterprises most certainly need
 
-Management Tools</p>
+experts to evaluate and configure the best tool for their purpose. We also offer services on other 
 
-<p>A complete API Management product can be very useful but is not mandatory to venture into APIs.
+commercial API Management Tools like Layer7, 3Scale, Mulesoft etc.</p>
+<p>A complete API Management product can be very useful but is not mandatory to venture into APIs. 
 
 Nevertheless, even the simplest API implementation will need ways to make the APIs discoverable, 
 
 understandable, and at least minimally secured.</p>
-
-<p>Simply get started with WaveMaker Gateway Lite. Postpone your worries about procuring a commercial
+<p>Simply get started with WaveMaker Gateway Lite. Postpone your worries about procuring a commercial 
 
 API Management tool even before you are sure about your APIs.</p>
-
-<p>You may have APIs but creating API driven smart web & mobile applications is time consuming 
+<img src="images/wavemaker/logo-gateway-lite.png" />
+<p class="italics flt-left" style="margin:10px 0px 0px 40px;padding-left:40px; border-left:1px solid #ccc; font-style:italic;">WM Gateway Lite is a free to use light weight API management solution.</p>
+<h5>Fast-track web and mobile app development</h5>
+<p>You may have APIs but creating API driven smart web & mobile applications is time consuming
 
 and difficult without the right tools.</p>
-<p>Many enterprises are in a state that is unable to realize the investments made in APIs. Using a tool 
+<p>Many enterprises are in a state that is unable to realize the investments made in APIs. Using a tool
 
-like Wavemaker Stydio, creating API driven applications to be served on web and mobile is no longer 
+like Wavemaker Studio, creating API driven applications to be served on web and mobile is no longer 
 
 time consuming or difficult. The Studio is a visual RAD tool that offers you ways to drag-and-drop UI 
 
-components, connect to web services, and create database schemas -- without writing any code.</p>
+components, connect to web services, create database schemas and much more-- without writing any 
 
+code.</p>
+<img src="images/wavemaker/logo-studio.png" />
 <h5>Our Experience with APIs</h5>
+<p>Imaginea has been serving as the engineering arm for the WaveMaker product suite. Building a 
 
+commercial grade API Management Gateway and other related products has given us immense insight 
+
+in to different aspect of API building, publishing and consuming.</p>
 <p>WaveMaker, Inc. provides App Enablement Suite (AES), an end-to-end solution for publishing, managing 
 
 APIs and uniquely promoting API consumption. WaveMaker improves API consumption by empowering 
 
 the large population of user developers, as well as professional developers, to create API apps.</p>
+<p>WaveMaker is a portfolio company of Pramati Technologies. The product DNA in Imaginea has always
 
-<p>WaveMaker is a portfolio company of Pramati Technologies. Imaginea has been serving as the 
+ensured high quality deliverables and very happy customers.</p>
+<p>Want to be happy too? Discuss your API needs with our experts.</p>
 
-engineering arm for the WaveMaker product suite. Building a commercial grade API Management 
+<img style="max-width:100%;"src="images/api-lifecycle.png" />
 
-Gateway has given us immense insight in to different aspect of API building, publishing and consuming.</p> 
 
-<p>The product DNA in Imaginea has always ensured high quality deliverables and very happy customers. </p>
-
-<p>Want to be happy too?</p>
 
 
 
